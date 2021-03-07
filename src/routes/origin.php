6464d58f957538e33d81a19a8b7d52e643be74af
Gabriel Santos <barricaspt@gmail.com>
2021-03-07 02:44:44 +0000

    Update flags when starting/stopping streams

--- src/routes/origin.php
+++ src/routes/origin.php
@@ -4,9 +4,10 @@ use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-use App\Models\Stream;
-
 use \Illuminate\Database\Eloquent\ModelNotFoundException;
+use Carbon\Carbon;
+
+use App\Models\Stream;
 
 return function (App $app) {
     $app->post('/origin/publish', function(Request $request, Response $response) {
@@ -14,12 +15,35 @@ return function (App $app) {
         $token = $request->getParam('token');
 
         try {
-            (new Stream())
+            $stream = (new Stream())
                 ->where('name', $streamName)
                 ->where('token', $token)
                 ->firstOrFail();
+
+            $stream->is_streaming = true;
+            $stream->last_stream_at = Carbon::now();
+
+            $stream->save();
         } catch(ModelNotFoundException $e) {
             return $response->withStatus(403);
+        }
+
+        return $response->withStatus(200);
+    });
+
+    $app->post('/origin/publish-done', function(Request $request, Response $response) {
+        $streamName = $request->getParam('name');
+
+        try {
+            $stream = (new Stream())
+                ->where('name', $streamName)
+                ->firstOrFail();
+
+            $stream->is_streaming = false;
+
+            $stream->save();
+        } catch(ModelNotFoundException $e) {
+            return $response->withStatus(404);
         }
 
         return $response->withStatus(200);
